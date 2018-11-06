78e7e07b25ddaacce84abba66212b64160b8dbde
leamaraa <leafabiano74@gmail.com>
2018-11-06 15:08:57 +0100

    inizio

--- sketch.js
+++ sketch.js
@@ -1,11 +1,65 @@
-function preload(){
-  // put preload code here
+let particles = [];
+var mic;
+
+function preload() {
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  mic = new p5.AudioIn();
+
+
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  mic.start();
+  let p = new Particle();
+  particles.push(p);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+    if (particles[i].toDelete())
+      //remove particles
+
+      particles.splice(i, 2);
+
+
+  }
+
+}
+
+class Particle {
+
+  constructor() {
+    this.x = windowWidth / 2;
+    this.y = windowHeight / 2;
+    var ciao = mic.getLevel();
+    this.vx = random(-ciao,ciao);
+    this.vy = random(-ciao,ciao);
+    this.alpha = 255;
+  }
+
+  toDelete() {
+    return this.alpha < 0;
+  }
+
+  update() {
+    this.x += this.vx*5;
+    this.y += this.vy*5;
+    this.alpha -= 5;
+
+  }
+
+  show() {
+    noStroke();
+    stroke(255);
+    fill(255, this.alpha);
+    ellipse(this.x, this.y, 50)
+  }
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
